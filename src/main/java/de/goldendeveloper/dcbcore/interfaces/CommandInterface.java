17a8f06a5214b8fbb3ebcfbfc7b9420a88bd1b76
c.ilgner <c.ilgner@tmg-web.de>
2023-05-22 11:30:33 +0200

    ADD: hasRole as default Methode

--- src/main/java/de/goldendeveloper/dcbcore/interfaces/CommandInterface.java
+++ src/main/java/de/goldendeveloper/dcbcore/interfaces/CommandInterface.java
@@ -1,17 +1,23 @@
 package de.goldendeveloper.dcbcore.interfaces;
 
 import de.goldendeveloper.dcbcore.DCBot;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 
+@SuppressWarnings("unused")
 public interface CommandInterface {
 
     CommandData commandData();
 
     void runSlashCommand(SlashCommandInteractionEvent e, DCBot dcBot);
 
-    @SuppressWarnings("unused")
     default void runCommandAutoComplete(CommandAutoCompleteInteractionEvent e, DCBot dcBot) {
+    }
+
+    default boolean hasRole(Role role, Member m) {
+        return m.getRoles().stream().filter(r -> r == role).map(r -> true).findFirst().orElse(false);
     }
 }
