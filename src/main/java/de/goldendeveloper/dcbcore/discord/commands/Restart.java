c82c48ca619885415e62b7649dfb5fba2a81c3de
c.ilgner <c.ilgner@tmg-web.de>
2024-06-03 12:54:36 +0200

    FIX: Restart Command

--- src/main/java/de/goldendeveloper/dcbcore/discord/commands/Restart.java
+++ src/main/java/de/goldendeveloper/dcbcore/discord/commands/Restart.java
@@ -7,8 +7,10 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Restart implements CommandInterface {
@@ -31,14 +33,58 @@ public class Restart implements CommandInterface {
     public static void restartBot(JDA jda, DCBot dcBot) {
         try {
             String jarFileName = dcBot.getConfig().getProjektName() + "-" + dcBot.getConfig().getProjektVersion() + ".jar";
-            String absolutePath = Paths.get(".").toAbsolutePath().normalize() + File.separator + jarFileName;
-            String[] commands = {"screen -AmdS " + dcBot.getConfig().getProjektName() + " java -Xms1096M -Xmx1096M -jar " + absolutePath + " restart"};
-            Process p = Runtime.getRuntime().exec(commands);
-            p.waitFor();
+            Path path = Paths.get(".").toAbsolutePath().normalize();
+            Process process = getProcess(dcBot, path, jarFileName);
+
+            try (InputStream errorStream = process.getErrorStream();
+                 BufferedReader errorReader = new BufferedReader(new InputStreamReader(errorStream));
+                 InputStream inputStream = process.getInputStream();
+                 BufferedReader inputReader = new BufferedReader(new InputStreamReader(inputStream))) {
+
+                String line;
+                System.out.println("Standard Output:");
+                while ((line = inputReader.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+                System.out.println("Error Output:");
+                while ((line = errorReader.readLine()) != null) {
+                    System.err.println(line);
+                }
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("Process exited with code: " + exitCode);
+
             jda.shutdown();
         } catch (Exception exception) {
             Sentry.captureException(exception);
             System.out.println(exception.getMessage());
         }
+    }
+
+    private static @NotNull Process getProcess(DCBot dcBot, Path path, String jarFileName) throws IOException {
+        String absolutePath = path + File.separator + jarFileName;
+
+        String screenPath = "/usr/bin/screen";
+        String javaPath = "/usr/bin/java";
+
+        String[] commands = {
+                screenPath,
+                "-AmdS",
+                dcBot.getConfig().getProjektName(),
+                javaPath,
+                "-Xms1096M",
+                "-Xmx1096M",
+                "-jar",
+                absolutePath,
+                "restart"
+        };
+
+        ProcessBuilder processBuilder = new ProcessBuilder(commands);
+        processBuilder.directory(new File(String.valueOf(path)));
+
+        Process process = processBuilder.start();
+        return process;
     }
 }
